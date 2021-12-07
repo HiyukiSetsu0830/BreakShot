25a253d82dfcf396f5db5e87aa333359041663a5
ykimura0830 <ykimura_work0830@gmail.com>
2021-12-07 18:40:21 +0900

    移動・ジャンプ完了
    移動とジャンプの実装

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,66 +9,54 @@ public class PlayerController : MonoBehaviour
 
     //プレイヤーのアニメーションを入れる
     [SerializeField] private Animator playerAnimator;
-    //プレイヤーのRigidbodyを入れる
     [SerializeField] private Rigidbody playerRigidbody;
 
     //前方向の速度
-    private float speed = 2f;
+    private float speed = 10f;
     //上方向の速度
-    private float jumpPower = 5f;
-    //ジャンプ可能かどうか
-    bool canJump = false;
+    private float jumpPower = 9f;
     //地面接触判定
-    private bool isGround = false;
-    //プレイヤーの座標
-    private Vector3 playerPos;
-    //プレイヤー初期位置
-    private const float startPosZ = -20f;
-    private const float startPosY = 0.25f;
-    
-    //移動キーの判定
-    bool getJButtonDown = false;
-    bool getRButtonDown = false;
-    bool getLButtonDown = false;
-   
+    private bool isGround;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //最初の時点でのプレイヤーのポジションを取得
-        playerPos = GetComponent<Transform>().position;    
+          
         //Animatorを取得
         playerAnimator = GetComponent<Animator>();
-        //Rigidbodyを取得
-        playerRigidbody = GetComponent<Rigidbody>();       
+        //RigidBodyの取得
+        playerRigidbody = GetComponent<Rigidbody>();
 
     }
 
     // Update is called once per frame
     void Update() {
 
-       
-        //移動キー
-        float isJumpButton = Input.GetAxis("Jump");
-        float isMoveButton = Input.GetAxis("Horizontal");
+        //Jumpステートの場合はJumpにfalseをセットする
+        bool isJumpSetFalse = this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("jumpBool");
+        this.playerAnimator.SetBool("jumpBool", false);
 
-        if (canJump) {
-            canJump = false;
-            playerRigidbody.velocity += new Vector3(0f, jumpPower * isJumpButton * Time.deltaTime, 0f);
+        //移動キー
+        bool isJumpButton = Input.GetButton("Jump");
+        float isMoveButton = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+
+        if (isGround && isJumpButton) {
+            this.playerAnimator.SetBool("jumpBool", true);
+            playerRigidbody.velocity = new Vector3(0f, jumpPower, 0f);
+            isGround = false;
         }
 
-        playerRigidbody.velocity += new Vector3(0f, 0f, speed * isMoveButton * Time.deltaTime);
-        //プレイヤーの座標更新
-        playerPos = transform.position;
+       //左右移動
+        transform.position += new Vector3(0f, 0f, isMoveButton);
+      
     }
 
     private void OnCollisionEnter(Collision collision) {
 
-        
         bool isGrounded = collision.gameObject.CompareTag("Ground");
         if (isGrounded) {
-            canJump = true;
+            isGround = true;
         }
     }
 }
